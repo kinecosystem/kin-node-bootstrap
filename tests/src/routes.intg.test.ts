787293d109abe5676c6c5e4ce02bd708d73bb8d6
Ravit Bar <ravit.bar@ecosystem.com>
2019-09-08 21:59:43 +0300

    Improved the tests + error handling

--- tests/src/routes.intg.test.ts
+++ tests/src/routes.intg.test.ts
@@ -1,24 +1,26 @@
 import {createApp} from "../../src/app";
-import {Channels, Environment, KeyPair, KinAccount, KinClient} from "@kinecosystem/kin-sdk-node";
+import {Channels, KeyPair, KinAccount, KinClient} from "@kinecosystem/kin-sdk-node";
 import {getKinAccount} from "../../src/init";
-import {TransactionNotFoundError} from "../../src/errors";
+import {
+	AccountNotFoundError,
+	InvalidParamError,
+	LowBalanceError,
+	MissingParamError,
+	TransactionNotFoundError
+} from "../../src/errors";
 import {ANON_APP_ID, INTEGRATION_ENVIRONMENT, MEMO_CAP, MEMO_TEMPLATE, VERSION} from "../environment";
 import {ConfigParams} from "../../src/config/environment";
 
 const request = require('supertest');
-
-interface ConfigTestParams extends ConfigParams {
-	FRIEND_BOT: string | undefined;
-}
 
 describe('Test routes', () => {
 	let app: any;
 	let account: KinAccount;
 	let keyPair: KeyPair;
 	let keyPairs: KeyPair[];
-	let config: ConfigTestParams;
+	let config: ConfigParams;
 	let client: KinClient;
-	const destination = 'GB26SXD5FXCGHZJKZQARJBYBG43YYSWB6LUBMSQ4JCA3W6QRV7KMBSTE';
+	const destination = KeyPair.generate().publicAddress;
 
 	beforeEach(async () => {
 		keyPair = KeyPair.generate();
@@ -32,7 +34,6 @@ describe('Test routes', () => {
 			CHANNEL_SALT: 'bootstrap',
 			CHANNEL_STARTING_BALANCE: 1,
 			PORT: 3000,
-			FRIEND_BOT: Environment.Testnet.friendbotUrl,
 			CONSOLE_LOGGER: 'SILLY'
 		};
 
@@ -40,6 +41,11 @@ describe('Test routes', () => {
 		await client.friendbot({
 			address: keyPair.publicAddress,
 			amount: config.CHANNEL_STARTING_BALANCE * config.CHANNEL_COUNT + 1000
+		});
+
+		await client.friendbot({
+			address: destination,
+			amount: 1000
 		});
 
 		keyPairs = await Channels.createChannels({
@@ -76,12 +82,7 @@ describe('Test routes', () => {
 	test('Get Balance - account not found', async () => {
 		const keyPair = KeyPair.generate();
 		const response = await request(app).get(`/balance/${keyPair.publicAddress}`);
-		const data = JSON.parse(response.text);
-		expect(data.http_code).toEqual(404);
-		expect(data.code).toEqual(4041);
-		expect(data.status).toEqual(404);
-		expect(data.title).toEqual('Not Found');
-		expect(data.message).toEqual(`Account '${keyPair.publicAddress}' was not found`);
+		expect(response.text).toEqual(AccountNotFoundError(keyPair.publicAddress).toString());
 	}, 120000);
 
 	test('Get Payment - successful', async () => {
@@ -104,7 +105,7 @@ describe('Test routes', () => {
 
 	test('Get Payment - wrong transaction', async () => {
 		const response = await request(app).get('/payment/5d68a90441be50285d24fcfcea7a93bd6a2fee97d3cf7a9d46c62a7c2b1ad350');
-		expect(response.text).toEqual(JSON.stringify(TransactionNotFoundError('5d68a90441be50285d24fcfcea7a93bd6a2fee97d3cf7a9d46c62a7c2b1ad350')));
+		expect(response.text).toEqual(TransactionNotFoundError('5d68a90441be50285d24fcfcea7a93bd6a2fee97d3cf7a9d46c62a7c2b1ad350').toString());
 	}, 120000);
 
 	test('Post Pay - successful', async () => {
@@ -144,14 +145,7 @@ describe('Test routes', () => {
 			amount: -150,
 			memo: 'pay-failed'
 		});
-		const data = JSON.parse(response.text);
-
-		expect(data.http_code).toEqual(400);
-		expect(data.code).toEqual(4001);
-		expect(data.title).toEqual('Bad Request');
-		expect(data.http_code).toEqual(400);
-		expect(data.status).toEqual(400);
-		expect(data.message).toEqual('Amount for payment must be bigger than 0');
+		expect(response.text).toEqual(InvalidParamError('Amount for payment must be bigger than 0').toString());
 	}, 120000);
 
 	test('Post Pay - wrong address', async () => {
@@ -160,14 +154,7 @@ describe('Test routes', () => {
 			amount: 150,
 			memo: 'pay-failed'
 		});
-		const data = JSON.parse(response.text);
-
-		expect(data.http_code).toEqual(400);
-		expect(data.code).toEqual(4001);
-		expect(data.title).toEqual('Bad Request');
-		expect(data.http_code).toEqual(400);
-		expect(data.status).toEqual(400);
-		expect(data.message).toEqual(`Destination '${destination + 'A'}' is not a valid public address`);
+		expect(response.text).toEqual(InvalidParamError(`Destination '${destination + 'A'}' is not a valid public address`).toString());
 	}, 120000);
 
 	test('Post Pay - too long memo', async () => {
@@ -177,14 +164,7 @@ describe('Test routes', () => {
 			amount: 150,
 			memo: memo
 		});
-		const data = JSON.parse(response.text);
-
-		expect(data.http_code).toEqual(400);
-		expect(data.code).toEqual(4001);
-		expect(data.title).toEqual('Bad Request');
-		expect(data.http_code).toEqual(400);
-		expect(data.status).toEqual(400);
-		expect(data.message).toEqual(`Memo: '${memo}' is longer than ${MEMO_CAP}`);
+		expect(response.text).toEqual(InvalidParamError(`Memo: '${memo}' is longer than ${MEMO_CAP}`).toString());
 	}, 120000);
 
 	test('Post Pay - balance too low', async () => {
@@ -195,8 +175,7 @@ describe('Test routes', () => {
 			amount: balance * 2,
 			memo: 'pay-successful'
 		});
-		const data = JSON.parse(response.text);
-		expect(data.message).toEqual('The account does not have enough kin to perform this operation');
+		expect(response.text).toEqual(LowBalanceError().toString());
 	}, 120000);
 
 	test('Post Pay - missing param', async () => {
@@ -204,9 +183,7 @@ describe('Test routes', () => {
 			destination: destination,
 			memo: 'pay-successful'
 		});
-
-		const data = JSON.parse(response.text);
-		expect(data.message).toEqual('The parameter \'amount\' was missing from the requests body');
+		expect(response.text).toEqual(MissingParamError('The parameter \'amount\' was missing from the requests body').toString());
 	}, 120000);
 
 	test('Post Create - successful', async () => {
@@ -235,7 +212,6 @@ describe('Test routes', () => {
 		const payData = JSON.parse(payResponse.text);
 		const history = await client.getRawTransactionData(payData.tx_id);
 		expect(history.source === keyPairs[0].publicAddress.toString() || keyPairs[1].publicAddress.toString()).toBeTruthy();
-		expect(1).toEqual(1);
 	}, 120000);
 
 	test('Post Create - wrong address', async () => {
@@ -246,14 +222,7 @@ describe('Test routes', () => {
 			starting_balance: startingBalance,
 			memo: 'create-successful'
 		});
-		const data = JSON.parse(response.text);
-
-		expect(data.http_code).toEqual(400);
-		expect(data.code).toEqual(4001);
-		expect(data.title).toEqual('Bad Request');
-		expect(data.http_code).toEqual(400);
-		expect(data.status).toEqual(400);
-		expect(data.message).toEqual(`Destination '${keyPair.publicAddress + 'A'}' is not a valid public address`);
+		expect(response.text).toEqual(InvalidParamError(`Destination '${keyPair.publicAddress + 'A'}' is not a valid public address`).toString());
 	}, 120000);
 
 	test('Post Create - negative starting balance', async () => {
@@ -263,14 +232,7 @@ describe('Test routes', () => {
 			starting_balance: -300,
 			memo: 'create-successful'
 		});
-		const data = JSON.parse(response.text);
-
-		expect(data.http_code).toEqual(400);
-		expect(data.code).toEqual(4001);
-		expect(data.title).toEqual('Bad Request');
-		expect(data.http_code).toEqual(400);
-		expect(data.status).toEqual(400);
-		expect(data.message).toEqual('Starting balance for account creation must not be negative');
+		expect(response.text).toEqual(InvalidParamError('Starting balance for account creation must not be negative').toString());
 	}, 120000);
 
 	test('Post Create - too long memo', async () => {
@@ -281,13 +243,6 @@ describe('Test routes', () => {
 			starting_balance: 300,
 			memo: memo
 		});
-		const data = JSON.parse(response.text);
-
-		expect(data.http_code).toEqual(400);
-		expect(data.code).toEqual(4001);
-		expect(data.title).toEqual('Bad Request');
-		expect(data.http_code).toEqual(400);
-		expect(data.status).toEqual(400);
-		expect(data.message).toEqual(`Memo: '${memo}' is longer than ${MEMO_CAP}`);
+		expect(response.text).toEqual(InvalidParamError(`Memo: '${memo}' is longer than ${MEMO_CAP}`).toString());
 	}, 120000);
 });
